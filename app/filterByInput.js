8c4833587d1028c5dbf4bcc2006cf23ab52b1689
Lucas Fabiano <lucas.fabiano@capco.com>
2022-05-24 20:39:14 -0300

    Separando arquivos javascript

---
+++ app/filterByInput.js
@@ -0,0 +1,31 @@
+angular.module('bookmarkApp').
+filter('filterByInput', function() {
+  function break3Letters(filterInput) {
+    let res = [];
+    while (filterInput.length > 3)  {
+      res.push(filterInput.substring(0, 3));
+      filterInput = filterInput.substring(3);
+    }
+    res.push(filterInput);
+    return res;
+  }
+  function contains(full, filterInput) {
+    let words = break3Letters(filterInput.toLowerCase());
+    let found = true;
+    for (let i = 0; i < words.length; i++) {
+      if (full.toLowerCase().indexOf(words[i]) < 0) {
+        found = false;
+        break;
+      }
+    }
+    return found;
+  }
+
+  return function(arr, filterInput) {
+    if (!filterInput) return arr;
+    return arr.filter(item => {
+      return contains(item.title, filterInput);
+    })
+
+  }
+})
