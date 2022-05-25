e9b4a0a2d3e970bd837dde87ab0619eec9a51127
Lucas Fabiano <lucas.fabiano@capco.com>
2022-05-25 14:22:35 -0300

    Adicionando bookmark

--- app/bookmark.controller.js
+++ app/bookmark.controller.js
@@ -3,12 +3,20 @@ controller('BookmarkController', function BookmarkController($scope) {
   const vm = $scope;
 
   vm.selected = 0;
+  vm.newItem = {};
 
   let bookmarks = localStorage.getItem("bookmarks");
+  console.log(`localStorage.setItem("bookmarks", ${bookmarks})`);
   if (!bookmarks) {
     bookmarks = [];
   } else {
     bookmarks = JSON.parse(bookmarks);
+  }
+
+  vm.saveItem = function(item) {
+    vm.bookmarks.push(item);
+    localStorage.setItem("bookmarks", JSON.stringify(vm.bookmarks));
+    vm.newItem = {};
   }
 
   vm.bookmarks = bookmarks;
